a5c88de7f7a656841ef3b5f91a53c294f076f5b6
Glenn Adams <flgadams@gmail.com>
2022-07-30 15:08:22 -0400

    Initial Commit

---
+++ template-swift-no-sb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  template-swift-no-sb
+//
+//  Created by Glenn Adams on 7/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
